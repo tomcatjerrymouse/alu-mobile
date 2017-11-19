55602d50f05dadd9ad25707c410203c58f0fa871
1178359532 <1178359532@qq.com>
2017-11-19 21:22:23 +0800

    122

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import logo from './官网移动端(2).jpg'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+          <img src={logo} width='100%' />
+      </div>
+    );
+  }
+}
+
+export default App;
